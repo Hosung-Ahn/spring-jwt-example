058a253d6647221638703e4034528332ea02c5eb
Hosung <an3735297@pusan.ac.kr>
2024-06-07 14:07:04 +0900

    feat : user details
    사용자가 아이디, 비밀번호를 통해 로그인 요청이 있을 때 인증 처리를 하는데 사용됩니다.

---
+++ jwt-example/src/main/java/com/example/jwtexample/security/userDetails/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.jwtexample.security.userDetails;
+
+import com.example.jwtexample.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    private Member member;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+        this.authorities = member.getAuthorities().stream().map(
+                authority -> new SimpleGrantedAuthority(authority.getName())
+        ).toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

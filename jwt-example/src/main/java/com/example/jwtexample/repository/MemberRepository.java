958f8cec307ea8948af064216f8074c67f4f64f6
Hosung <an3735297@pusan.ac.kr>
2024-06-07 13:53:29 +0900

    feat : member repo

---
+++ jwt-example/src/main/java/com/example/jwtexample/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.jwtexample.repository;
+
+import com.example.jwtexample.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}

ac76bdb1cbe61bc1d0d4d043c91d23e82a601d4e
Hosung <an3735297@pusan.ac.kr>
2024-06-28 07:55:14 +0900

    feat : spring security error handling

--- jwt-example/src/main/java/com/example/jwtexample/config/SecurityConfig.java
+++ jwt-example/src/main/java/com/example/jwtexample/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.jwtexample.config;
 
 import com.example.jwtexample.domain.EAuthority;
+import com.example.jwtexample.jwt.JwtFilter;
+import com.example.jwtexample.jwt.exception.JwtAccessDeniedHandler;
+import com.example.jwtexample.jwt.exception.JwtAuthenticationEntryPoint;
 import com.example.jwtexample.security.userDetails.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +17,16 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtFilter jwtFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -29,6 +36,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                        .exceptionHandling((exceptionHandling) -> exceptionHandling
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint));
 
         http.authorizeHttpRequests(authorize ->
                 authorize.requestMatchers("/user").hasAuthority(EAuthority.ROLE_USER.name())
